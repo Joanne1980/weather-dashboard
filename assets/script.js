fed74eee029a2d1ba257b05545de43503f99db49
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-03-20 16:50:23 +0000

    history code and prepend

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,7 @@ loadHistory();
 $("#search-form").on("submit", function (event) {
   event.preventDefault();
 
-//Button clicked and search done and stored
+  //Button clicked and search done and stored
   const button = $(this);
   if (button.attr("data-city")) {
     showWeather(button.attr("data-city"));
@@ -31,45 +31,86 @@ $("#search-form").on("submit", function (event) {
       showWeather(userInput);
     }
   }
-})
+});
 
 // function to show weather results
 function showWeather(userInput) {
- clear()
+  clear();
+  //prepend the value to the list container
+  $(".container-fluid.loader").show();
 
-})
+  //API key
+  const queryURL =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    userInput +
+    "&limit=5&appid=" +
+    apiKey;
+
+  //Call Geocode API when search form is submitted
+  $.ajax({ url: queryURL }).then(function (response) { 
+
+    $(".container-fluid.loader").hide()
+    if (response.length ! == 0){
+    
+    const lat = response[0].lat;
+    const lon = response[0].lon;
+
+    renderToday(lat, lon)
+    processForecast(lat, lon)
+    } else {
+      showPopup('${userInput} Please try again')
+    }
+  })
+}
+ function updateHistory(userInput) {
+
+ //Add the history to local storage
+  history.push(userInput);
+  localStorage.setItem("history", JSON.stringify(history));
+
+  loadHistory()
+
+ } 
+
+ function loadHistory() {
+
+  historySearch.empty()
+ if (JSON.parse(localStorage.getItem('history'))) {
+  JSON.parse(localStorage.getItem('history')).forEach(function(Element) {
+ const button = $("<button>").addClass("btn mt-2 button").text(element).attr("data-city"), element)
+  historySearch.prepend(button)
+ })
+}
+}
+
+ 
+ 
+
+  //   const weatherQueryUrl =
+  //     "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+  //     lat +
+  //     "&lon=" +
+  //     lon +
+  //     "&appid=" +
+  //     apiKey;
+  //   }
+  // });
+
+  
+  // $.ajax({ url: weatherQueryUrl })
+  // .then(function (weatherResponse) {
+
+  //   //Put the response on the HTML page
+  //   const weatherList = weatherResponse.list;
+
+  //   // Now forcast
+  //   const today = weatherList[0];
+  //   console.log(today);
+  
 
 // // TODO style the current HTML
 
-//
-//  const queryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput +
-//   '&limit=5&appid=' + apiKey;
 
-//   //Todo prepend the value to the list container
-
-//   // Add the history to local storage
-
-//   history.push(userInput);
-//   localStorage.setItem('history',JSON.stringify(history));
-
-//   //Call Geocode API when search form is submitted
-//   $.ajax({url: queryURL})
-//   .then(function(response) {
-//     const lat = response[0].lat;
-//     const lon = response[0].lon;
-
-//     const weatherQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
-//   });
-//   });
-
-//   $.ajax({ url: weatherQueryUrl })
-
-//   .then(function(weatherResponse) {
-//     //Put the response on the HTML page
-//     const weatherList = weatherResponse.list;
-//     // Now forcast
-//     const today = weatherList[0];
-//     console.log(today);
 
 //     //5 day forcast
 
